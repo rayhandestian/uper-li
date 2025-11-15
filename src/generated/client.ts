d5a12075769e993101503a984c3b434a915bb985
rayhandestian <rayhandes7@gmail.com>
2025-11-15 16:59:33 +0700

    fix: use includeFiles at root level in vercel.json to include src/generated

--- src/generated/client.ts
+++ src/generated/client.ts
@@ -48,6 +48,10 @@ path.join(process.cwd(), "src/generated/query_engine-windows.dll.node")
 path.join(__dirname, "libquery_engine-rhel-openssl-3.0.x.so.node")
 path.join(process.cwd(), "src/generated/libquery_engine-rhel-openssl-3.0.x.so.node")
 
+// file annotations for bundling tools to include these files
+path.join(__dirname, "libquery_engine-linux-musl-openssl-3.0.x.so.node")
+path.join(process.cwd(), "src/generated/libquery_engine-linux-musl-openssl-3.0.x.so.node")
+
 /**
  * Model User
  * 
