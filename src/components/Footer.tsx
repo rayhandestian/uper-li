bff711fc3c2bc6f8c8ecac4f2a8fa853372be2f5
rayhandestian <rayhandes7@gmail.com>
2025-11-17 12:19:21 +0700

    feat(ui): add link to copyright in footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,12 @@ export default function Footer() {
       <div className="max-w-7xl mx-auto py-12 px-6 sm:px-8 lg:px-8">
         <div className="flex flex-col md:flex-row justify-between items-center">
           <div className="text-base text-gray-500 mb-6 md:mb-0">
-            © 2025 UPer.li
+            <a
+              href="https://app.uper.li"
+              className="text-base text-gray-500 hover:text-gray-900 transition-colors"
+            >
+              © 2025 UPer.li
+            </a>
           </div>
           <div className="flex flex-col sm:flex-row space-y-4 sm:space-y-0 sm:space-x-8">
             <a
