2a2b1308f50348b22795e2c7cdddafae7c702f23
rayhandestian <rayhandes7@gmail.com>
2025-11-28 18:01:06 +0700

    fix(lint): add eslint-disable for require in generate-secret.js

--- scripts/generate-secret.js
+++ scripts/generate-secret.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
+/* eslint-disable @typescript-eslint/no-require-imports */
 /**
  * Generate a cryptographically secure secret for NEXTAUTH_SECRET
  * Usage: npm run generate-secret [length]
